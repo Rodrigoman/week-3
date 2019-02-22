b2112f17af45978e561d70172e0b3e4229e7bca5
= <=>
2019-02-21 18:59:42 -0600

    save and edit context

--- src/context.js
+++ src/context.js
@@ -1,23 +1,44 @@
 import Home from './home-page.js';
 import CreateEdit from './create-edit-page.js';
 
+
 class Context {
   constructor(state) {
-    this.changeState(state);
+    this.currentUrl = this.getUrlParams(window.location.hash);
+    switch (true) {
+      case this.currentUrl['#blog'] === 'new':
+        this.changeState('create', this.currentUrl);
+        break;
+      case !isNaN(this.currentUrl['#blog']):
+        this.changeState('create', this.currentUrl);
+        break;
+      default:
+        this.changeState('home');
+        break;
+    }
+    console.log(this.currentUrl);
   }
 
-  changeState(state) {
+  changeState(state, params) {
     switch (state) {
       case 'home':
         this.state = new Home();
         break;
       case 'create':
-        this.state = new CreateEdit();
+        this.state = new CreateEdit(params);
 
         break;
       default:
         this.state = new Home();
     }
+  }
+
+  getUrlParams(url) {
+    const hashes = url.slice(url.indexOf('?') + 1).split('&');
+    return hashes.reduce((params, hash) => {
+      const [key, val] = hash.split('=');
+      return Object.assign(params, { [key]: decodeURIComponent(val) });
+    }, {});
   }
 
   request() {
